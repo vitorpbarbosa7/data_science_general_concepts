b3eb6690323b62eb9573f72120f9a8d8b1e49264
vitor <vitorpbarbosa7@gmail.com>
2021-09-07 17:02:03 -0300

    struct cpp intro

---
+++ _data_structures_cpp/_struct.cpp
@@ -0,0 +1,14 @@
+
+
+# include <bits/stdc++.h>
+using namespace std;
+
+/// You're able to define a data type structure
+
+struct Books
+{
+  char Title[20];
+  char Author[20];
+  int ID;
+  float price;
+}book1;
