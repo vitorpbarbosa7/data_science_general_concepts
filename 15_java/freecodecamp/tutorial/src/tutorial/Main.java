cc84124818f0820a2c4f9694184e0b569a573627
vitor <vitorpbarbosa7@gmail.com>
2022-03-12 00:24:44 -0300

    java intro

---
+++ 15_java/freecodecamp/tutorial/src/tutorial/Main.java
@@ -0,0 +1,32 @@
+/* Name of general package we are working with tutorial */
+package tutorial;
+
+/* Main class that can receive other other methods
+ * instantiated from classes you previous declared*/
+public class Main {
+
+	/*  public - method can be accessed outside?
+	 *  static - 
+	 *  void - receives nothing
+	 *  main - method 
+	 *  String[] to declare args as String classes
+	 *  [] without worring about the number of arguments
+	 *  as in *args in python 
+	 *  [] is equivalent to *
+	 *  args equivalent to *args in python, could be any name?
+	 *  */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* Hello World*/
+		/* System.out.println("Hello World"); */
+		
+		/* */
+		int hello_world = 5;
+		double num2 = 5.0;
+		boolean b = false;
+		/*
+		System.out.println(hello_world);
+		System.out.println(num2);
+		System.out.println(b);*/
+	}
+}
