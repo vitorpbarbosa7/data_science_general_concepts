722a7dcd8ed9ff58cc3f7eac077b88a9e7e7ad65
Vitor Pereira Barbosa <vitorpbarbosa7@gmail.com>
2022-07-29 10:32:23 -0300

    produtorio construido

--- 40_maximum_likelihood_estimator/statquest/mle.py
+++ 40_maximum_likelihood_estimator/statquest/mle.py
@@ -3,6 +3,7 @@ import numpy as np
 from math import pi as pi
 from math import exp as exp
 import matplotlib.pyplot as plt
+from functools import reduce
 
 def likelihood_function(mean, sd, x):
 	a = 1/(np.sqrt(2*pi*(sd**2)))
@@ -20,9 +21,19 @@ likelihood_values = []
 for (mean, sd, x) in zip(means, sds, xs):
 	likelihood_values.append(likelihood_function(mean, sd, x))
 
-plt.plot(likelihood_values)
+# plt.plot(likelihood_values)
+# plt.show()
+
+def product_likelihood(mean, sd, distribuition:list):
+	single_likelihood = []
+	for point in distribuition:
+		single_likelihood.append(likelihood_function(mean, sd, point))
+	return reduce(lambda x, y: x*y, single_likelihood)	
+
+distribuition = [32, 34]
+products = []
+for (mean, sd) in zip(means, sds):
+	products.append(product_likelihood(mean, sd, distribuition))
+
+plt.plot(products)
 plt.show()
-
-
-
-
